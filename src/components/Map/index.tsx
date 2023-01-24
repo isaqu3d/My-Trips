30190925269d918d006f68e1e50c9afb35723edf
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-24 15:12:41 -0300

    styles: add new styles

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,13 +1,17 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
- const Map = () => {
+const Map = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={3} style={{height:'100%', width: '100%'}}>
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[0, 0]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
@@ -16,4 +20,4 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
   );
 };
 
-export default Map
+export default Map;
