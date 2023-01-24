b70959f2acca1545cf827b7a1f4faf7146c1cb38
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-24 12:09:46 -0300

    styles: add new styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+:root  {
+  --highlight: #e20e8d;
+  --background: #030518;
+  --white: #eeeeee;
+
+  --container: 100rem;
+
+  --small: 1.5rem;
+  --medium: 3rem;
+  --large: 5rem;
+}
+
   * {
     margin: 0;
     padding: 0;
@@ -13,11 +25,22 @@ const GlobalStyles = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background: var(--background);
+    color: var(--white);
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
-`
 
-export default GlobalStyles
+  p, a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
+  }
+`;
+
+export default GlobalStyles;
