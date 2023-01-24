fc4da3699cb6ddd8592dc0b53ed593f9e4513c53
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-24 14:33:55 -0300

    feat: add link leaflet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,13 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossOrigin=""
+        />
+
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
