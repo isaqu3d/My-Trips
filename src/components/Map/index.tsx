f3abd8a90ceb059e50a8357084f976f19ba271b7
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-25 17:14:02 -0300

    feat: add router and add eventHandlers in Marker

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 
 type Place = {
@@ -15,6 +16,8 @@ export type MapProps = {
 };
 
 const Map = ({ places }: MapProps) => {
+  const router = useRouter();
+
   return (
     <MapContainer
       center={[0, 0]}
@@ -25,7 +28,7 @@ const Map = ({ places }: MapProps) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {places?.map(({ id, name, location }) => {
+      {places?.map(({ id, slug, name, location }) => {
         const { latitude, longitude } = location;
 
         return (
@@ -33,6 +36,11 @@ const Map = ({ places }: MapProps) => {
             key={`place-${id}`}
             position={[latitude, longitude]}
             title={name}
+            eventHandlers={{
+              click: () => {
+                router.push(`/place/${slug}`);
+              },
+            }}
           />
         );
       })}
