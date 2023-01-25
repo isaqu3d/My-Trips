46039548f440ab8f035176399f510034ff509294
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-25 18:32:15 -0300

    feat: add next config img domains

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   swcMinify: true,
   experimental: {
     // Enables the styled-components SWC transform
-    styledComponents: true
+    styledComponents: true,
   },
   pwa: {
     dest: 'public',
-    disable: !isProd
-  }
-})
+    disable: !isProd,
+  },
+  images: {
+    domains: ['media.graphassets.com'],
+  },
+});
