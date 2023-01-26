1a63a06f71e66af87da2aae270aa6646ba242759
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-26 11:12:37 -0300

    feat: add NextSeo

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -3,12 +3,19 @@ import dynamic from 'next/dynamic';
 
 import LinkWrapper from 'components/LinkWrapper';
 import { MapProps } from 'components/Map';
+import { NextSeo } from 'next-seo';
 
 const Map = dynamic(() => import('components/Map'), { ssr: false });
 
 export default function HomeTemplate({ places }: MapProps) {
   return (
     <>
+      <NextSeo
+        title="My trips"
+        description="A simple project to show in a map the  places that i went and show more informations and photos when clicked"
+        canonical="https://isaque-my-trips.vercel.app"
+      />
+
       <LinkWrapper href="about">
         <InfoOutline size={32} aria-label="Icon About" />
       </LinkWrapper>
