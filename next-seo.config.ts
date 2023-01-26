349bdabaf86adf6bf3be8bdfcf083df0b5726f13
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-26 11:10:38 -0300

    feat: create config

---
+++ next-seo.config.ts
@@ -0,0 +1,14 @@
+import { NextSeoProps } from 'next-seo';
+
+const seo: NextSeoProps = {
+  openGraph: {
+    type: 'website',
+    locale: 'pt-BR',
+    url: 'https://isaque-my-trips.vercel.app',
+    title: 'My Trips',
+    description:
+      'This is my website where I put all the places I visited and that I would like to visit, I put some photos and things I like about these places.',
+  },
+};
+
+export default seo;
