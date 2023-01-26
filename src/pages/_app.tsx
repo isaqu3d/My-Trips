f67d998a57ce1ccf3ad7d328b3c9a29c6d125809
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-26 10:28:20 -0300

    feat: add NProgressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import NextNProgress from 'nextjs-progressbar';
 
 import GlobalStyles from 'styles/global';
 
@@ -25,6 +26,12 @@ function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <GlobalStyles />
+      <NextNProgress
+        color="#f231a5"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <Component {...pageProps} />
     </>
   );
